2e669738c73a6843eb3a11d503b1ddeabf6adb3e
said10 <bensamdisaid@gmail.com>
2018-07-31 13:50:41 +0100

    add UI-css

--- src/app.js
+++ src/app.js
@@ -8,17 +8,25 @@ class App extends Component {
     render() {
         return `
             <desktop>
-                <div id="app">
-                    <div class="background">
-                        <h1>It Worked!</h1>
+                <div id="app" class="theme-bleu">
+                    <div class="background box30 shadow">
+                        <div class="box10">
+                            <h1>It Worked!</h1>
+                        </div>
+                        
                         <p>Welcome to invock-js Framework :)</p>
                     </div>
                 </div>
             </desktop>
             <mobile>
-                <div class="background-mobile">
-                    <h1>It Worked!</h1>
-                    <p>Welcome to invock-js Framework :)</p>
+                <div id="app" class="theme-violet">
+                    <div class="background box30 shadow">
+                        <div class="box10">
+                            <h1>It Worked!</h1>
+                        </div>
+                        
+                        <p>Welcome to invock-js Framework :)</p>
+                    </div>
                 </div>
             </mobile>
         `;
